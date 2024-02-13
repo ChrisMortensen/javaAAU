f5d9eea8034f5b112327350ac82e68c0535453d5
Christian Mortensen <christian@svalgaard.net>
2024-02-13 14:54:23 +0100

    Lesson 2

---
+++ lesson2/CleaningSystem.java
@@ -0,0 +1,20 @@
+public class CleaningSystem {
+	public static void main(String[] args) {
+		navigate();
+		collectGarbage();
+		navigate();
+	}
+	private static void navigate(){
+		detectObstacles();
+		
+	}
+	private static void detectObstacles(){
+
+	}
+	private static void collectGarbage(){
+		disposeGarbage();
+	}
+	private static void disposeGarbage(){
+	}
+
+}
---
+++ lesson2/TheSystem.java
@@ -0,0 +1,9 @@
+public class TheSystem {
+	public static void main(String[] args) {
+		//	When a table is input:
+		requestCleaning();
+	}
+	private static void requestCleaning(){
+
+	}
+}
